7f5560cdfc86321c20d2dc9c549d6e7aff45edc3
Hugo Ramon <hramonp-dev@outlook.com>
2025-09-30 20:53:46 -0300

    Added Zustand and created the global state

--- app/(store)/product/[slug]/page.tsx
+++ app/(store)/product/[slug]/page.tsx
@@ -1,3 +1,5 @@
+import AddToCartButton from "@/components/AddToCartButton";
+import { Button } from "@/components/ui/button";
 import { imageUrl } from "@/lib/imageUrl";
 import { getProductBySlug } from "@/sanity/lib/products/getProductBySlug";
 import { PortableText } from "next-sanity";
@@ -45,6 +47,10 @@ async function ProductPage({ params }: { params: Promise<{ slug: string }> }) {
                 <PortableText value={product.description} />
               )}
             </div>
+          </div>
+          <div className="mt-6">
+            <AddToCartButton product={product} disabled={isOutOfStock} />
+            <Button>Add to Cart</Button>
           </div>
         </div>
       </div>
